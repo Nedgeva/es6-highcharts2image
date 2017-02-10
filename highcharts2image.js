b68e47ab576e63241e0d4a61bcf3ac3792bfe77a
Nedgeva <s3psis@gmail.com>
2017-02-10 10:37:44 +0100

    Initial release

---
+++ highcharts2image.js
@@ -0,0 +1,222 @@
+/**
+ * highCharts2Image v1.0.0 by Nedgeva
+ * 'Render Highcharts/Highstock plots to image on client side without any hassle'
+ * ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+ * @param {object} options - Main highCharts2Image options object
+ * @param {object} options.chartOptions - Highcharts/Highstock options
+ * @param {string} options.chartEngine - use 'highcharts' or 'highstock' plot engine (default is 'highcharts')
+ * @param {string} options.chartEngineVersion - Highcharts/Highstock engine version (default is '5.0.7')
+ * @param {function} options.chartCallback - pass callback function with `chart` as single argument (default is `chart => chart.redraw()`)
+ * @param {string} options.iframeId - specify id for temporary created iframe by highCharts2Image lib (default is pseudo-random GUID used as iframe id)
+ * @param {number} options.width - specify width in pixels for output image (default is `600`)
+ * @param {number} options.height - specify width in pixels for output image (default is `400`)
+ * @return {Promise<string>} - Base64 encoded PNG image
+ * @throws {Promise<string>} - Error explanation
+ */
+
+const highCharts2Image = options =>
+  new Promise((resolve, reject) => {
+
+    // GUID generator for
+    // pseudo-random iframe id
+    const pseudoGuid = () => {
+      const fourChars = () => {
+        return ( ((1 + Math.random()) * 0x10000) | 0 )
+          .toString(16)
+          .substring(1)
+          .toUpperCase()
+      }
+      
+      return fourChars()
+           + fourChars()
+           + "-" + fourChars()
+           + "-" + fourChars()
+           + "-" + fourChars()
+           + "-" + fourChars()
+           + fourChars()
+           + fourChars()
+    }
+
+    // setting defaults, if any option is omitted
+    const opts = Object.assign({
+      chartEngine: 'highcharts',
+      chartEngineVersion: '5.0.7',
+      chartCallback: chart => { /* do anything with chart object */ },
+      iframeId: pseudoGuid(),
+      width: 600,
+      height: 400
+    }, options)
+
+    const { 
+      chartEngine,
+      chartOptions,
+      chartEngineVersion,
+      chartCallback,
+      iframeId,
+      width,
+      height
+    } = opts
+
+    // escape from promise with iframe removing
+    const exitGracefully = (msg, isRejected) => {
+      window.removeEventListener('message', onmessage)
+      const hc2imageFrame = window.document.getElementById(iframeId)
+      hc2imageFrame.parentElement.removeChild(hc2imageFrame)
+      return isRejected
+        ? reject(msg)
+        : resolve(msg)
+    }
+
+    // set distro urls
+    const distroUrl = 'https://cdnjs.cloudflare.com/ajax/libs/highcharts'
+
+    const distroObj = {
+      highcharts: `${distroUrl}/${chartEngineVersion}/highcharts.js`,
+      highstock: `${distroUrl}/${chartEngineVersion}/highstock.js`,
+      exporting: `${distroUrl}/${chartEngineVersion}/js/modules/exporting.js`,
+      offlineExporting: `${distroUrl}/${chartEngineVersion}/js/modules/offline-exporting.js`
+    }
+
+    const chartMethodObj = {
+      highcharts: 'chart',
+      highstock: 'stockChart'
+    }
+
+    // define injector fn to ensure sequential script loading
+    const injectr = (doc, srcList) => {
+      if (srcList.length === 0)
+        return
+
+      const script = doc.createElement('script')
+      script.type = 'text/javascript' 
+      
+      const source = srcList[0]
+      const srcListNew = srcList.slice(1)
+
+      script.onload = e =>
+        injectr(doc, srcListNew)
+
+      script.onerror = e =>
+        exitGracefully(`Error: can't load script: ${source.src}`, true)
+
+      if (source.src)
+        script.src = source.src
+      else if (source.text)
+        script.textContent = source.text
+      else 
+        return
+
+      doc.body.appendChild(script)
+    }
+
+    const fillFrame = () => {
+      
+      // convert payload fn to string
+      // that will be eval'd inside iframe
+      const payloadFn = (() => {
+        const win = window.frames.parent
+        
+        // post messages to parent window via window.postMessage()
+        const postBack = (png, errMsg) =>
+          win.postMessage({
+            from: '$FRAMEID',
+            png,
+            errMsg
+          }, '*')
+
+        const getImageFromSVG = svg =>
+          Highcharts.imageToDataUrl(
+            Highcharts.svgToDataUrl(svg),
+            'image/png',
+            { /* empty */ },
+            1,
+            postBack
+          )
+        
+        try {
+          
+          // set/override renderTo option
+          const options = Object.assign($OPTIONS, {
+            chart: {
+              renderTo: 'container'
+            }
+          })
+          
+          // draw chart
+          const chart = new Highcharts.$CHARTMETHOD( options )
+
+          // pass chart object to callback function
+          const cbResult = ( $CALLBACK )(chart)
+
+          // encode svg chart to png image
+          chart.getSVGForLocalExport(null, null, null, getImageFromSVG)
+  
+        } catch(err) {  
+          
+          // post back error message via window.postMessage()
+          postBack(null, err.toString())
+          
+        }
+      })
+        .toString()
+        .replace('$FRAMEID', iframeId)
+        .replace('$OPTIONS', JSON.stringify(chartOptions))
+        .replace('$CHARTMETHOD', chartMethodObj[chartEngine.toLowerCase()])
+        .replace('$CALLBACK', chartCallback.toString())
+
+
+      const HTMLMarkup =
+        `
+          <div id="container" style="width: ${width}px; height: ${height}px;"></div>
+        `
+      
+      const payloadJS =
+        `
+          eval( (${payloadFn})() )
+        `
+
+      const injScriptList = [
+        {src: distroObj[chartEngine.toLowerCase()]},
+        {src: distroObj.exporting},
+        {src: distroObj.offlineExporting},
+        {text: payloadJS}
+      ]
+      
+      const doc = window.frames[iframeId].document
+      doc.body.insertAdjacentHTML('beforeend', HTMLMarkup)
+      injectr(doc, injScriptList)
+    }
+
+    // create iframe
+    const iframe = document.createElement('iframe')
+    iframe.id = iframeId
+    iframe.name = iframeId
+    iframe.style = 'width: 1px; height: 1px; visibility: hidden;'
+    iframe.onload = fillFrame
+
+    // append iframe
+    document.body.appendChild(iframe)
+    
+    // deploy conflict-free 'window.onmessage' listener
+    const onmessage = message => {
+      const { 
+        from,
+        png,
+        errMsg
+      } = message.data
+      
+      // skip messages from other frames
+      if (from !== iframeId)
+        return
+
+      return png !== null
+        ? exitGracefully(png)
+        : exitGracefully(errMsg, true)
+    }
+
+    window.addEventListener('message', onmessage)
+
+  })
+
+// expose highCharts2Image function to window
+window.highCharts2Image = highCharts2Image
---
+++ test/spec.js
@@ -0,0 +1,246 @@
+'use strict';
+
+const chartOptions = {
+  title: {
+     text: ''
+  },
+  xAxis: {
+     categories: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec']
+  },
+  series: [{
+     data: [29.9, 71.5, 106.4, 129.2, 144.0, 176.0, 135.6, 148.5, 216.4, 194.1, 95.6, 54.4]
+  }],
+  navigation: {
+    buttonOptions: {
+      align: 'center'
+    }
+  }
+}
+
+describe('Testing highCharts2Image internal features', () => {
+
+  const elToRemove = {
+    context: null,
+    child: null
+  }
+
+  const oldRemoveChild = Element.prototype.removeChild
+
+  before(() => {
+
+    function removeChildHooked(child) {
+      elToRemove.context = this
+      elToRemove.child = child
+    }
+
+    Element.prototype.removeChild = removeChildHooked
+
+    const options = { chartOptions }
+
+    return highCharts2Image(options)
+
+  })
+
+  after(() => {
+    oldRemoveChild.call(elToRemove.context, elToRemove.child)
+    Element.prototype.removeChild = oldRemoveChild
+  })
+
+  it('Should be successfully loaded and window-wide accessible', () => {
+    expect(window.highCharts2Image).to.be.a('function')
+  })
+
+  it('Should create hidden 1x1 iframe tag', () => {
+    const iframes = document.querySelectorAll('iframe')
+    expect(iframes).to.have.length(1)
+    expect(iframes[0].style.width).to.be.equal('1px')
+    expect(iframes[0].style.height).to.be.equal('1px')
+    expect(iframes[0].style.visibility).to.be('hidden')
+  })
+
+  it('Should embed all necessary scripts inside iframe', () => {
+    const frame = document.querySelector('iframe')
+    const embeddedScripts = Array.from(
+      frame.contentDocument.querySelectorAll('script')
+    )
+    
+    const sources = embeddedScripts
+      .filter(script => script.src)
+      .map(script => {
+        const slashIndex = script.src.lastIndexOf('/') + 1
+        return script.src.slice(slashIndex, script.src.length)
+      })
+    
+    const isEmbedded = ['highcharts.js', 'exporting.js', 'offline-exporting.js']
+      .every(libName => sources.includes(libName))
+
+    expect(embeddedScripts).to.have.length(4)
+    expect(isEmbedded).to.be.ok()
+
+  })
+
+  it('Should have Highcharts object available inside iframe window', () => {
+    const frame = document.querySelector('iframe')
+    const winFrame = frame.contentWindow || frame
+    expect(winFrame.Highcharts).to.be.a('object')
+  })
+
+  it('Should append div container inside iframe', () => {
+    const frame = document.querySelector('iframe')
+    const divContainer = frame.contentDocument.querySelector('div#container')
+    expect(divContainer).to.have.property('nodeName', 'DIV')
+  })
+
+})
+
+describe('Testing highCharts2Image external features', () => {
+
+  it('Should resolve proper base64 encoded image', () => {
+
+    const isBase64 = str => {
+      try {
+        const commaIndex = str.indexOf(',') + 1
+        const base64 = str.slice(commaIndex, str.length)
+        return btoa(atob(base64)) == base64;
+      } catch (err) {
+        return false;
+      }
+    }
+
+    const options = {
+      chartOptions,
+      width: 1,
+      height: 1
+    }
+
+    return highCharts2Image(options)
+      .then(result =>
+        expect(isBase64(result)).to.be(true)
+      )
+
+  })
+
+  it('Should resolve proper base64 encoded image with desired dimension', () => {
+
+    const options = {
+      chartOptions,
+      width: 800,
+      height: 600
+    }
+
+    const testImgOnLoad = e => {
+      expect(e.target.width).to.be.equal(options.width)
+      expect(e.target.height).to.be.equal(options.height)
+    }
+
+    return highCharts2Image(options)
+      .then(result => {
+        const img = document.createElement('img')
+        img.src = result
+        img.onload = testImgOnLoad
+      })
+
+  })
+
+  it('Should resolve same result repeatedly', () => {
+    
+    const options = {
+      chartOptions,
+      width: 200,
+      height: 400
+    }
+
+    const hcInstance = highCharts2Image(options)
+
+    const promiseList = new Array(5)
+      .fill(hcInstance)
+
+    return Promise.all(promiseList)
+      .then(resultList => {
+        const isResultsAreSame = resultList
+          .every((v, i, a) => v === a[0])
+
+        expect(resultList).to.have.length(5)
+        expect(isResultsAreSame).to.be.ok()
+      })
+
+  })
+
+  it('Should allow multiple instances which resolves correct results', () => {
+
+    const shuffle = arr =>
+      arr
+        .slice()
+        .sort(() =>
+          Math.random() >= 0.5
+        )
+
+    const options = {
+      chartOptions,
+      width: 200,
+      height: 400
+    }    
+
+    const chartDataList = chartOptions.series[0].data
+
+    const optionList = new Array(5)
+      .fill(options)
+
+    const promiseList = optionList
+      .map(option => {
+        option.chartOptions.series[0].data = shuffle(chartDataList)
+        return highCharts2Image(option)
+      })
+
+    return Promise.all(promiseList)
+      .then(resultList => {
+        const isResultsAreSame = resultList
+          .every((v, i, a) => v === a[0])
+
+        expect(resultList).to.have.length(5)
+        expect(isResultsAreSame).not.to.be.ok()
+      })
+
+  })
+
+  it('Should reject on invalid highcharts options', () => {
+
+    const options = {
+      chartOptions: {
+        series: 'invalid data for demo purposes'
+      }
+    }
+
+    return highCharts2Image(options)
+      .catch(reason =>
+        expect(reason).to.contain('Error')
+      )
+
+  })
+
+  it('Should reject on invalid highcharts script version', () => {
+
+    const options = {
+      chartOptions,
+      chartEngineVersion: '666'
+    }
+
+    return highCharts2Image(options)
+      .catch(reason =>
+        expect(reason).to.contain('Error')
+      )
+
+  })
+
+  it('Should not leave any iframe tags', () => {
+    
+    const options = { chartOptions }
+
+    return highCharts2Image(options)
+      .then(result =>
+        expect(document.querySelectorAll('iframe')).to.have.length(0)
+      )
+
+  })
+
+})
